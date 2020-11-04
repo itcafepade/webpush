a8d5b456ab49853c0de1acf5124740cb3cfaaeb4
Leonel López <leonellopez647@gmail.com>
2020-11-03 19:42:14 -0600

    Notifications

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes/index');
+const path = require('path');
+//Get .env variables
+require('dotenv').config();
 
+//Initialize the server
 const app = express();
-
-const server = app.listen('3000', ()=>{
-    console.log("Listen to port 3000");
-});
 
 //Middleware
 app.use(morgan('dev'));
@@ -15,3 +15,10 @@ app.use(express.json());
 
 //Routes
 app.use(router);
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')))
+        
+app.listen('3000', ()=>{
+    console.log("Listen to port 3000");
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,28 @@
+//Get the variables in the .env
+require('dotenv').config();
+
 const {Router} = require('express');
+const webpush = require('web-push');
+const wbp = require('../webpush');
+let pushSuscription;
 
 const router = Router();
+
+router.post('/suscribe', async (req, res)=>{
+    console.log(req.body);
+    pushSuscription = req.body; 
+    const payload = JSON.stringify({
+        title: "Hola",
+        message: "Hola mensaje"
+    });
+    try {
+        await webpush.sendNotification(pushSuscription, payload)
+    } catch (error) {
+        
+    }
+    res.status(200).json();
+});
+
 
 
 module.exports = router;

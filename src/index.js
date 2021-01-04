ded02ff6d93278458c7ef386af0d397bcf0a864e
Leonel López <leonellopez647@gmail.com>
2021-01-04 14:25:27 -0600

    Dinamyc notifications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const router = require('./routes/index');
 const path = require('path');
+
 //Get .env variables
 require('dotenv').config();
 
@@ -10,7 +12,9 @@ const app = express();
 
 //Middleware
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.json());
 
 //Routes
@@ -18,7 +22,7 @@ app.use(router);
 
 //Static files
 app.use(express.static(path.join(__dirname, 'public')))
-        
-app.listen('3000', ()=>{
+
+app.listen('3000', () => {
     console.log("Listen to port 3000");
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,34 @@
 //Get the variables in the .env
 require('dotenv').config();
 
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
 const webpush = require('web-push');
 const wbp = require('../webpush');
 let pushSuscription;
 
 const router = Router();
 
-router.post('/suscribe', async (req, res)=>{
-    console.log(req.body);
-    pushSuscription = req.body; 
-    const payload = JSON.stringify({
-        title: "Hola",
-        message: "Hola mensaje"
-    });
+router.post('/suscribe', async (req, res) => {
     try {
-        await webpush.sendNotification(pushSuscription, payload)
+        pushSuscription = req.body.dataSuscription;
+        // console.log(req.body.dataSuscription)
+        const payload = await JSON.stringify({
+            title: req.body.title,
+            message: req.body.message
+        });
+
+        try {
+            await webpush.sendNotification(pushSuscription, payload);
+        } catch (error) {
+            console.log('Error: ' + error);
+        }
     } catch (error) {
-        
+        console.log('Error try: ' + error);
+        res.status(300).json();
     }
     res.status(200).json();
 });
-
-
 
 module.exports = router;
